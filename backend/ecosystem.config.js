68a65b8d7d6d9b70369ab8a8d994aad6c9688bbb
Tema-Bash <cel-ir@yandex.ru>
2023-07-11 20:18:54 +0300

    deploy backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env.deploy" });
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY } =
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } =
   process.env;
 
 module.exports = {
@@ -17,11 +17,22 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: DEPLOY_REPOSITORY,
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `bash scrtips/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      "post-deploy":
-        "cd backend && pwd&& npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+      "pre-deploy-local": `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      "post-deploy": `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 restart all`,
     },
   },
+  // deploy: {
+  //   production: {
+  //     user: DEPLOY_USER,
+  //     host: DEPLOY_HOST,
+  //     ref: DEPLOY_REF,
+  //     repo: DEPLOY_REPOSITORY,
+  //     path: DEPLOY_PATH,
+  //     "pre-deploy-local": `bash scrtips/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+  //     "post-deploy":
+  //       "cd backend && pwd&& npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+  //   },
+  // },
 };
